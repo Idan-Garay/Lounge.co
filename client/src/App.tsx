b7d90964853e80e37ef414a46376db756b8b036a
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-04-30 20:16:50 +0800

    fix: add layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,14 @@ import "./App.scss"
 
 const App = () => {
   return (
-    <div className='app'>very nice </div>
+    <div className='app'>
+      <aside className='message-menu'>
+
+      </aside>
+      <main className='message-window'>
+        
+      </main>
+    </div>
   )
 }
 
