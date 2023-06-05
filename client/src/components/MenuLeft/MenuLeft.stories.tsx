82bbcc2986fce1dacae0c5b06eabda90e6f3293c
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-06-05 15:42:27 +0800

    fix: finalize design and functionality

---
+++ client/src/components/MenuLeft/MenuLeft.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import MenuLeft from "./MenuLeft"
+
+export default {
+    component: MenuLeft,
+    title: "MenuLeft",
+}
+
+export const Default = {
+    args: {
+        profileImage: "https://images.pexels.com/photos/2078467/pexels-photo-2078467.jpeg?auto=compress&cs=tinysrgb&w=50&h=50&dpr=2",
+    }
+}
--- client/src/components/MenuLeft/MenuLeft.tsx
+++ client/src/components/MenuLeft/MenuLeft.tsx
@@ -1,36 +1,70 @@
-import React from 'react'
-import { IoChatbubbleEllipsesSharp} from "react-icons/io5"
+import React, { useEffect, useRef, useState } from 'react'
+import { IoChatbubbleEllipsesSharp } from "react-icons/io5"
 import { HiArchiveBox } from "react-icons/hi2";
 import { FaStore } from "react-icons/fa"
 import { TbMessageCircle2Filled } from "react-icons/tb"
+import { MdSettings } from "react-icons/md"
 
 import "./MenuLeft.scss"
 
-const MenuLeft = () => {
+interface MenuLeftProps {
+    profileImage: string
+}
+
+const MenuLeft = ({ profileImage }: MenuLeftProps) => {
+    const settingsElemRef = useRef<HTMLDivElement>(null)
+    const [toggleSettings, setToggleSettings] = useState(false)
+    const [currentItem, setCurrentItem] = useState("item 1")
+    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        setCurrentItem(e.currentTarget.dataset.itemName)
+    }
+    const handleToggleSettings = () => {
+        setToggleSettings((toggleSettings) => !toggleSettings)
+    }
+
+    useEffect(() => {
+        function handleClickOutside(event:MouseEvent) {
+            if (settingsElemRef.current && !settingsElemRef.current.contains(event.target as Node)) {
+                handleToggleSettings()
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside)
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [])
+
     return (
         <div className="menu-left">
             <div className="menu-top">
-                <div className="icon-space active">
-                    <TbMessageCircle2Filled size={20} className=" " />
+                <div className={`icon-space ${currentItem === "item 1" ? "active" : ""}`} data-item-name="item 1" onClick={handleClick}>
+                    <TbMessageCircle2Filled size={20} />
                 </div>
-                <div className="icon-space">
-                    <FaStore size={18} className=" " />
-                </div>
-
-                <div className="icon-space">
-                    <IoChatbubbleEllipsesSharp size={20} className=" " />
-
+                <div className={`icon-space ${currentItem === "item 2" ? "active" : ""}`} data-item-name="item 2" onClick={handleClick}>
+                    <FaStore size={18} />
                 </div>
 
-                <div className="icon-space">
-                    <HiArchiveBox size={20} className=" " />
+                <div className={`icon-space ${currentItem === "item 3" ? "active" : ""}`} data-item-name="item 3" onClick={handleClick}>
+                    <IoChatbubbleEllipsesSharp size={20} />
+
+                </div>
+
+                <div className={`icon-space ${currentItem === "item 4" ? "active" : ""}`} data-item-name="item 4" onClick={handleClick}>
+                    <HiArchiveBox size={20} />
                 </div>
             </div>
 
-            <div className="menu-bottom">
 
-                <div className="icon-space active">
-                    <div className="bottom-picture"></div>
+            <div className="menu-bottom" ref={settingsElemRef}>
+                <div className={`settings ${toggleSettings ? "" : "display-none"}`} >
+                    <div className="settings-background">
+                        <MdSettings size={20} />
+                        <p><b>Preferences</b></p>
+                    </div>
+                </div>
+                <div className="icon-space active" onClick={handleToggleSettings}>
+                    <img src={profileImage} alt="profile" className="bottom-picture" />
                 </div>
             </div>
         </div>
